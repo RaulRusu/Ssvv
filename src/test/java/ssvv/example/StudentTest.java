00dd5d062992193cfd43c38133352cda1ca2518d
Raul <raul.rusu@tracotsetgo.com>
2024-04-16 22:34:22 +0300

    test: studet BVA

--- src/test/java/ssvv/example/StudentTest.java
+++ src/test/java/ssvv/example/StudentTest.java
@@ -66,55 +66,29 @@ public class StudentTest
                 null);
     }
 
-//    @org.junit.jupiter.api.Test
-//    public void testAddStudentDuplicate()
-//    {
-//        Student student = new Student("8", "H", 8, "a@a");
-//        service.addStudent(student);
-//
-//        try {
-//            service.addStudent(student);
-//            fail();
-//        } catch (Exception e) {
-//            assertEquals(e.getMessage(), "Studentul exista deja!");
-//        }
-//    }
-
     @org.junit.jupiter.api.Test
     public void testAddStudent_Success()
     {
-        Student student = new Student("2", "A", 1, "a@a");
+        Student student = new Student("111", "test", 937, "t@t.com");
 
         try {
             service.addStudent(student);
 
             // Get the student from the repository
-            Student newStudent = xmlRepo.findOne("2");
+            Student newStudent = xmlRepo.findOne("111");
 
             assertEquals(student, newStudent);
         } catch (Exception e) {
             fail();
         }
 
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testAddStudent_EmptyId()
-    {
-        Student student = new Student("", "B", 2, "a@a");
-
-        try {
-            service.addStudent(student);
-            fail();
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Id incorect!");
-        }
+        service.deleteStudent("111");
     }
 
     @org.junit.jupiter.api.Test
     public void testAddStudent_NullId()
     {
-        Student student = new Student(null, "B", 2, "a@a");
+        Student student = new Student(null, "test", 937, "t@t.com");
 
         try {
             service.addStudent(student);
@@ -127,7 +101,7 @@ public class StudentTest
     @org.junit.jupiter.api.Test
     public void testAddStudent_EmptyName()
     {
-        Student student = new Student("3", "", 3, "a@a");
+        Student student = new Student("111", "", 937, "t@t.com");
 
         try {
             service.addStudent(student);
@@ -136,38 +110,11 @@ public class StudentTest
             assertEquals(e.getMessage(), "Nume incorect!");
         }
     }
-
-    @org.junit.jupiter.api.Test
-    public void testAddStudent_NullName()
-    {
-        Student student = new Student("4", null, 4, "a@a");
-
-        try {
-            service.addStudent(student);
-            fail();
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Nume incorect!");
-        }
-    }
-
 
     @org.junit.jupiter.api.Test
     public void testAddStudent_EmptyEmail()
     {
-        Student student = new Student("5", "E", 5, "");
-
-        try {
-            service.addStudent(student);
-            fail();
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Email incorect!");
-        }
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testAddStudent_NullEmail()
-    {
-        Student student = new Student("6", "F", 6, null);
+        Student student = new Student("111", "test", 937, "");
 
         try {
             service.addStudent(student);
@@ -180,7 +127,7 @@ public class StudentTest
     @org.junit.jupiter.api.Test
     public void testAddStudent_NegativeGroup()
     {
-        Student student = new Student("7", "G", -7, "a@a");
+        Student student = new Student("111", "test", -1, "t@t.com");
 
         try {
             service.addStudent(student);
@@ -188,5 +135,37 @@ public class StudentTest
         } catch (Exception e) {
             assertEquals(e.getMessage(), "Grupa incorecta!");
         }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testAddStudent_BoundaryValuesGroup_Error()
+    {
+        Student student = new Student("111", "test", -1, "t@t.com");
+
+        try {
+            service.addStudent(student);
+            fail();
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Grupa incorecta!");
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testAddStudent_BoundaryValuesGroup_Success()
+    {
+        Student student = new Student("111", "test", 0, "t@t.com");
+
+        try {
+            service.addStudent(student);
+
+            // Get the student from the repository
+            Student newStudent = xmlRepo.findOne("111");
+
+            assertEquals(student, newStudent);
+        } catch (Exception e) {
+            fail();
+        }
+
+        service.deleteStudent("111");
     }
 }
